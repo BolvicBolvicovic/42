b882466b909e3b13ac4eafd5910eecd6553441bd
Bolvic <victor.bolheme@gmail.com>
2024-01-20 19:38:34 +0100

    ex05 done

---
+++ cpp_pool/CPP1/ex05/src/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+int	main(void)
+{
+	std::string	input;
+	Harl		harl;
+
+	do
+	{
+		std::cout << "Enter a level: ";
+		std::cin >> input;
+		harl.complain(input);
+	} while (input.compare("e"));
+	return (0);
+}
