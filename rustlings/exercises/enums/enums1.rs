6267e852520f05e59e797465a4a0c3ec05e8a34d
Victor Cornille <vcornill@c1r1p5.42lehavre.fr>
2024-02-08 17:35:12 +0100

    update

--- rustlings/exercises/enums/enums1.rs
+++ rustlings/exercises/enums/enums1.rs
@@ -2,15 +2,18 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+	SEGFAULT,
+	Echo,
+	Move,
+	ChangeColor,
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::SEGFAULT);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::ChangeColor);
