80ac95ada573c7314e9397426171163df6cf4343
Victor Cornille <vcornill@c1r2p3.42lehavre.fr>
2024-03-19 17:10:28 +0100

    update

--- cpp_pool/CPP6/ex00/src/ScalarConverter.cpp
+++ cpp_pool/CPP6/ex00/src/ScalarConverter.cpp
@@ -2,24 +2,52 @@
 #include <iostream>
 #include <cstdlib>
 #include <climits>
-void	convert_char(char c) {
-	std::cout << "char  : " << c 						<< std::endl
-			  << "int   : " << static_cast<int>(c)		<< std::endl
-			  << "float : " << static_cast<float>(c)	<< ".0f" << std::endl
-			  << "double: " << static_cast<double>(c)	<< ".0f" << std::endl;
-}
-
-void	convert_else(char *str) {
-	double	num = atof(str);
-	if (!num) {
-		int	i = -1;
-		while (iswhitespace(str[i])) {
-			
-		}
-	}
-}
 
 void	ScalarConverter::convert(std::string str) {
-	if		(str.length() == 1)	{ convert_char(str.c_str()[0]);		}
-	else						{ convert_else(str.c_str());		}
+	int			toInt;
+	float		toFloat;
+	std::string	toChar;
+	double		toDouble;
+
+	std::string	[]infinity = ["+inf","-inf","nan","+inff","-inff","nanf"];
+	bool		isInfinite = false;
+	
+	toDouble	= atof(str.c_str());
+	toInt		= static_cast<int>(toDouble);
+	toFloat		= static_cast<float>(toDouble);
+
+	if (toInt >= 32 && toInt <= 126) {
+		toChar = "'" + static_cast<char>(toInt) + "'"; 
+	} else {
+		toChar = "impossible";
+	}
+
+	for (int i = 0; i <= 5; i++) {
+		if (infinity[i] == str)
+			isInfinite = true;
+	}
+
+	if (!isInfinite && toInt == 0 && str[0] != 0 && str[1] != 0 && literal[0] != '+' && literal[0] != '-') {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+		return;
+	}
+
+	if (isInfinite) {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+	} else {
+		std::cout << "char: " << toChar << std::endl;
+		std::cout << "int: " << toInt << std::endl;
+	}
+
+	if (toFloat - static_cast<int>(tFloat) == 0) {
+		std::cout << "float: "<< toFloat << ".0f" << std::endl;
+		std::cout << "double: "<< toDouble << ".0" << std::endl;
+	} else {
+		std::cout << "float: "<< toFloat << "f" << std::endl;
+		std::cout << "double: "<< toDouble << std::endl;
+	}
 }
