36d36443d0b05da8838c1e66baf8b058429670ff
Victor Cornille <vcornill@c1r2p5.42lehavre.fr>
2024-03-21 14:12:59 +0100

    update

--- cpp_pool/CPP7/ex01/src/iter.hpp
+++ cpp_pool/CPP7/ex01/src/iter.hpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 template <typename T>
-void	iter(T *arr, size_t len, void f(T &)) {
+void	iter(const T *arr, size_t len, void f(T &)) {
 	for (size_t i = 0; i < len; i++) {
 		f(arr[i]);
 	}
---
+++ cpp_pool/CPP8/ex00/src/easyfind.hpp
@@ -0,0 +1,11 @@
+#include <wcexcept.h>
+
+template <typename T>
+int		easyfind(T container, int n) {
+	int	size = constainer.size();
+	int i = -1;
+	while (++i < size)
+		if (container[i] == n)
+			return i;
+	throw 
+}
