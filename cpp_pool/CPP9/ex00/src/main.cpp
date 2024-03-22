72e6d63c05bd0a5313bead0684c7c776cb7d61df
Victor Cornille <vcornill@c1r2p5.42lehavre.fr>
2024-03-22 17:03:58 +0100

    update

---
+++ cpp_pool/CPP9/ex00/src/main.cpp
@@ -0,0 +1,12 @@
+#include "BitcoinExchange.cpp"
+#include <iostream>
+
+
+int	main(int ac, char **av) {
+	if (ac != 2) {
+		std::cerr << "Panic: Wrong argument!" << std::endl;
+		return 1;
+	}
+	
+	return 0;
+}
